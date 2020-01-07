678941403e88db8423db26fc9b08d43a07088e80
Eric Masiello <eric.j.masiello@gmail.com>
2020-01-07 17:52:48 -0500

    Update deps and add storybook

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+/* eslint-disable import/no-extraneous-dependencies */
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
+import svgr from '@svgr/rollup';
+import autoprefixer from 'autoprefixer';
 
-import pkg from './package.json'
+import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
@@ -14,26 +16,28 @@ export default {
     {
       file: pkg.main,
       format: 'cjs',
-      sourcemap: true
+      sourcemap: true,
     },
     {
       file: pkg.module,
       format: 'es',
-      sourcemap: true
-    }
+      sourcemap: true,
+    },
   ],
   plugins: [
     external(),
     postcss({
-      modules: true
+      modules: true,
+      extract: true,
+      minimize: !process.env.ROLLUP_WATCH,
+      plugins: [autoprefixer()],
     }),
     url(),
     svgr(),
     babel({
       exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
     }),
     resolve(),
-    commonjs()
-  ]
-}
+    commonjs(),
+  ],
+};
