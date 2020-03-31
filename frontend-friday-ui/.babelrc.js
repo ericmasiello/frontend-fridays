679600f39ec6aaa1043a9ae6408018b45e672758
Eric Masiello <eric.j.masiello@gmail.com>
2020-03-30 20:26:44 -0400

    Strip down babel config

--- frontend-friday-ui/.babelrc.js
+++ frontend-friday-ui/.babelrc.js
@@ -1,22 +1,3 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', {
-      modules: false,
-    }],
-    '@babel/preset-react',
-  ],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    ['transform-react-remove-prop-types', {
-      mode: 'unsafe-wrap',
-    }],
-  ],
-  env: {
-    test: {
-      presets: [
-        '@babel/preset-env',
-        '@babel/preset-react',
-      ],
-    },
-  },
+  presets: ['@babel/preset-env', '@babel/preset-react']
 };
